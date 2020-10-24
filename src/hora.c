794e400bbfc73cf3498056dfd61999e6c7dd1a2c
martinquiroz20 <martin.quirozq@alumnos.uv.cl>
2020-10-23 21:37:08 -0300

    Probando codigo 3

--- src/hora.c
+++ src/hora.c
@@ -2,23 +2,30 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <time.h>
+void tiempo(){
+	time_t horario; 
+	time(&horario);  //definimos la variable horario
+	printf("Señal SIGUSR1 recibida: %s\n", ctime(&horario)); //hacemos una funcion tiempo en donde nos muestra el horario del computador y lo imprime
+}
 void signal_handler( int signal_num ) { 
 	static int trials = 0;
 	trials++;
 	printf("Interrupt signal is (%d).\n", signal_num);
   	if(trials == 2){
-		exit(signal_num);  
+		exit(signal_num);   //Aca se hace la interrupcion, si apretamos 1 vez Ctrl+c se interrumpe, si son 2 el programa se sale
 	} 
 } 
 int main(int argc, const char *argv[])
 {
 	int count = 0;  
 	signal(SIGTERM, signal_handler);   
-	signal(SIGINT, signal_handler);    
+	signal(SIGINT, signal_handler);
+	signal(SIGUSR1, tiempo);    
 	printf("Programa hora ejecutandose. PID=%d.\n", getpid());   	  
 	while(1) { 
 		printf("Listo para recibir la señal SIGUSR1.\n");
-		sleep(2000);
+		sleep(2000);		
 	}	
 	return EXIT_SUCCESS;
 }
